46471962b7a278822a08a46d6f971dc9a7b57194
csmith55 <csmith@Conors-MacBook-Pro.local>
2015-01-29 13:28:05 +0000

    Post-Demonstration Commit
    First commit to version control. Allows versioning, use of branches and
    progress of work.
    Done prior to major developmental changes

---
+++ app/src/main/java/app/com/project/csmith/finalyearproject/DetailActivity.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.com.project.csmith.finalyearproject;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ShareActionProvider;
+import android.widget.TextView;
+
+import com.facebook.widget.ProfilePictureView;
+
+
+public class DetailActivity extends Activity {
+
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new DetailFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class DetailFragment extends Fragment {
+
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+
+        private static final String hashTagShare = " #Friends";
+        private String locationString;
+        private String text[] = new String[1];
+
+
+        public DetailFragment() {
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+
+            Intent intent = getActivity().getIntent();
+            if (intent != null && intent.hasExtra("FBNAMES") && intent.hasExtra("FBPICS")) {
+                locationString = intent.getStringExtra("FBNAMES");
+                text = locationString.split("Location:");
+                ((TextView) rootView.findViewById(R.id.detail_text))
+                        .setText(text[0]);
+
+                ((TextView) rootView.findViewById(R.id.checkins))
+                        .setText("12:00 - Checked in @ Queen's University\n" +
+                                "10/12/2014 - Checked in @ Newcastle, Co.Down\n" +
+                                "09/12/2014 - Checked in @ Movie House, Dublin Road\n" +
+                                "08/12/2014 - Checked in @ Victoria Square Shopping Centre\n" +
+                                "07/12/2014 - Checked in @ Belfast City Hall\n");
+
+                ProfilePictureView profilePictureView = (ProfilePictureView) rootView.findViewById(R.id.profile_pic);
+                profilePictureView.setProfileId(intent.getStringExtra("FBPICS"));
+                profilePictureView.setCropped(true);
+
+            }
+
+            return rootView;
+        }
+
+        private void openPreferredLocationInMap() {
+            SharedPreferences sharedPrefs =
+                    PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+            String textSplit = text[1];
+            String[] split = textSplit.split("Distance: ");
+            String location = split[0];
+
+            Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                    .appendQueryParameter("q", location)
+                    .build();
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(geoLocation);
+
+            if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                startActivity(intent);
+            } else {
+                Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+            }
+        }
+
+        @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.detailfragment, menu);
+
+
+            // Retrieve the share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            // Get the provider and hold onto it to set/change the share intent.
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) menuItem.getActionProvider();
+
+            // Attach an intent to this ShareActionProvider.  You can update this at any time,
+            // like when the user selects a new piece of data they might like to share.
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareIntent());
+            } else {
+                Log.d(LOG_TAG, "Share Action Provider is null?");
+            }
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+
+            int id = item.getItemId();
+
+            if (id == R.id.action_map) {
+                openPreferredLocationInMap();
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+        private Intent createShareIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,
+                    locationString + hashTagShare);
+            return shareIntent;
+        }
+    }
+}
---
+++ app/src/main/java/app/com/project/csmith/finalyearproject/MainFragment.java
@@ -0,0 +1,292 @@
+package app.com.project.csmith.finalyearproject;
+
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.ProfilePictureView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class MainFragment extends android.support.v4.app.Fragment {
+
+    private static final String TAG = "MainFragment";
+    private static final String FBPICS = "FBPICS";
+    private static final String FBNAMES = "FBNAMES";
+    private static String[] fakeLocations = {"BT10 0GR \nDistance: 100", "CastleCourt \nDistance: 150", "Lisburn \nDistance: 600", "BT7 1LQ \nDistance: 1500", "BT25 3PL \nDistance: 2500", "Aviva Stadium \nDistance: 3500"};
+    protected ArrayAdapter<String> profileName;
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
+    private UiLifecycleHelper uiHelper;
+    private ArrayList<String> profilePics = new ArrayList<String>();
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        uiHelper = new UiLifecycleHelper(getActivity(), callback);
+        uiHelper.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(getActivity(), SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void updateFriends() {
+        makeMeRequest(Session.getActiveSession());
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateFriends();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        profileName = new ArrayAdapter<>(getActivity(), R.layout.list_item, R.id.list_item_friend_textview, new ArrayList<String>());
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+        authButton.setFragment(this);
+        authButton.setReadPermissions(Arrays.asList("user_friends"));
+
+        // Find the user's profile picture custom view
+        // Get a reference to the ListView, and attach this adapter to it.
+
+        ListView listView = (ListView) view.findViewById(R.id.listview_friend);
+        listView.setAdapter(profileName);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                String name = profileName.getItem(position);
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(FBNAMES, name);
+                intent.putExtra(FBPICS, profilePics.get(position));
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Session session = Session.getActiveSession();
+        if (session != null &&
+                (session.isOpened() || session.isClosed())) {
+            onSessionStateChange(session, session.getState(), null);
+        }
+
+        uiHelper.onResume();
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    private void onSessionStateChange(final Session session, SessionState state, Exception exception) {
+        if (state.isOpened()) {
+            Log.i(TAG, "Logged in!");
+            updateFriends();
+
+        } else if (state.isClosed()) {
+            Log.i(TAG, "Logged out!");
+        }
+    }
+
+    public void makeMeRequest(final Session session) {
+        // Make an API call to get user data and define a
+        // new callback to handle the response.
+        SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        final String value = preference.getString("value", "10000");
+        final String type = preference.getString("units", "default value");
+
+        Request request = Request.newMyFriendsRequest(session,
+                new Request.GraphUserListCallback() {
+
+
+                    @Override
+                    public void onCompleted(List<GraphUser> graphUsers, Response response) {
+                        if (session == Session.getActiveSession()) {
+                            if (graphUsers != null) {
+
+                                // Set the id for the ProfilePictureView
+                                // view that in turn displays the profile picture.
+                                //  profilePictureView.setProfileId(graphUsers.get(0).getId());
+                                // Set the Textview's text to the user's name.
+                                // userNameView.setText(user.getName());
+                                profileName.clear();
+                                profilePics.clear();
+                                ProfilePictureView profilePictureView = null;
+
+                                if (type.contains("Nearest Friend")) {
+                                    int numOfFriends = Integer.parseInt(value);
+                                    if (numOfFriends > graphUsers.size())
+                                        numOfFriends = graphUsers.size();
+                                    for (int i = 0; i < numOfFriends; i++) {
+
+
+                                        profileName.add(graphUsers.get(i).getName() + "\nLocation: " + fakeLocations[i]);
+                                        profilePics.add(graphUsers.get(i).getId());
+
+                                        switch (i) {
+                                            case 0:
+                                                profilePictureView = (ProfilePictureView) getView().findViewById(R.id.profile_pic0);
+                                                break;
+                                            case 1:
+                                                profilePictureView = (ProfilePictureView) getView().findViewById(R.id.profile_pic1);
+                                                break;
+                                            case 2:
+                                                profilePictureView = (ProfilePictureView) getView().findViewById(R.id.profile_pic2);
+                                                break;
+                                            case 3:
+                                                profilePictureView = (ProfilePictureView) getView().findViewById(R.id.profile_pic3);
+                                                break;
+                                            case 4:
+                                                profilePictureView = (ProfilePictureView) getView().findViewById(R.id.profile_pic4);
+                                                break;
+                                        }
+
+
+                                        profilePictureView.setProfileId(graphUsers.get(i).getId());
+                                        profilePictureView.setCropped(true);
+
+                                    }
+                                } else {
+                                    for (int i = 0; i < graphUsers.size(); i++) {
+
+                                        String[] split = fakeLocations[i].split("Distance: ");
+                                        String distance = split[1];
+                                        int distanceOfFriend = Integer.parseInt(distance);
+                                        int maxDistance = Integer.parseInt(value);
+                                        if (distanceOfFriend <= maxDistance) {
+                                            profileName.add(graphUsers.get(i).getName() + "\nLocation: " + fakeLocations[i]);
+                                            profilePics.add(graphUsers.get(i).getId());
+
+                                            switch (i) {
+                                                case 0:
+                                                    profilePictureView = (ProfilePictureView) getView().findViewById(R.id.profile_pic0);
+                                                    break;
+                                                case 1:
+                                                    profilePictureView = (ProfilePictureView) getView().findViewById(R.id.profile_pic1);
+                                                    break;
+                                                case 2:
+                                                    profilePictureView = (ProfilePictureView) getView().findViewById(R.id.profile_pic2);
+                                                    break;
+                                                case 3:
+                                                    profilePictureView = (ProfilePictureView) getView().findViewById(R.id.profile_pic3);
+                                                    break;
+                                                case 4:
+                                                    profilePictureView = (ProfilePictureView) getView().findViewById(R.id.profile_pic4);
+                                                    break;
+                                            }
+
+
+                                            profilePictureView.setProfileId(graphUsers.get(i).getId());
+                                            profilePictureView.setCropped(true);
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+                        if (response.getError() != null) {
+                            // Handle errors, will do so later.
+                        }
+                    }
+
+
+                });
+        request.executeAsync();
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
